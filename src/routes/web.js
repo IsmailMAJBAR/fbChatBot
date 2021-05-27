ad0e5a9f630a58355799acf2a3c10173bdab5160
majbar ismail <majbarismail@gmail.com>
2021-05-28 01:26:51 +0200

    configuring messenger dev first layer

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,14 @@
 import express from "express";
+import chatBotController from "../controllers/chatBotController";
 import homePageController from "../controllers/homePageController";
 
 let router = express.Router();
 
 let initWebRoutes =(app)=>{
  router.get("/", homePageController.getHomePage);
+ router.get("/webhook", chatBotController.getWebhok);
+ router.post("/webhook", chatBotController.postwebHook);
+
     return app.use("/", router);
 
 };
